5666ca8ebd5b1e56af11f32259676f59b868d967
Emil Lindblom <emillindblom@MacBook-Pro-2.local>
2020-10-02 10:17:56 +0200

    updated script.js, added comments

--- Beacon Example/script.js
+++ Beacon Example/script.js
@@ -1,20 +1,3 @@
-/*
- * @license
- * Getting Started with Web Serial Codelab (https://todo)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
 "use strict";
 
 let port;
@@ -35,16 +18,13 @@ document.addEventListener("DOMContentLoaded", () => {
   butIbeacon.addEventListener("click", clickIbeacon);
   butEddystone.addEventListener("click", clickEddystone);
   butConnect.addEventListener("click", clickConnect);
-
-  // CODELAB: Add feature detection here.
-  // CODELAB: Add feature detection here.
   const notSupported = document.getElementById("notSupported");
   notSupported.classList.toggle("hidden", "serial" in navigator);
 });
 
 /**
  * @name connect
- * Opens a Web Serial connection to a micro:bit and sets up the input and
+ * Opens a Web Serial connection to a serial device such as a Smart USB Dongle 2.0 and sets up the input and
  * output stream.
  */
 async function connect() {
@@ -64,7 +44,11 @@ async function connect() {
   );
 
   reader = inputStream.getReader();
-  readLoop();
+  readLoop().catch((error) => {
+    toggleUIConnected(false);
+    port = null;
+    log.textContent = "Dongle Disconnected!";
+  });
 }
 
 /**
@@ -89,13 +73,18 @@ async function disconnect() {
   // Close the port.
   await port.close();
   port = null;
+  log.textContent = "Dongle Disconnected!";
 }
 
 /**
  * @name clickConnect
  * Click handler for the connect/disconnect button.
+ * Checks if port != null
+ * If true: Checks if any beacons is advertising and stops the advertsing if so. Then runs disconnect() and set toggleUIConnected to false.
+ * if false: Runs connect() then set toggleUIConnected to true.
  */
 async function clickConnect() {
+  log.textContent = "";
   if (port) {
     if (isEddystonesAdv || isIbeaconAdv) {
       writeCmd("AT+ADVSTOP");
@@ -112,10 +101,14 @@ async function clickConnect() {
 
 /**
  * @name clickIbeacon
- * Click handler for the connect/disconnect button.
+ * Click handler for the iBeacon button.
+ * Checks if an iBeacon is already running by checking the boolean isIbeaconAdv.
+ * If isIbeaconAdv = true: Stops advertising, changes the button text and shows the Eddystone button. Finally sets isEddystoneAdv = false.
+ * If isIbeaconAdv = false: Sets the advertising data to setup an iBeacon with a UUID and starts advertising.
+ * Also changes button text and hides the Eddystone button. Finally sets isIbeaconAdv = true.
  */
 function clickIbeacon() {
-  console.log("IBEACON BUTTON");
+  console.log("IBEACON BUTTON PRESSED");
 
   if (isIbeaconAdv) {
     writeCmd("AT+ADVSTOP");
@@ -128,7 +121,8 @@ function clickIbeacon() {
   writeCmd("AT+ADVDATAI=5f2dd896-b886-4549-ae01-e41acd7a354a0203010400");
   setTimeout(() => {
     writeCmd("AT+ADVSTART=0;200;3000;0;");
-  }, 500);
+  }, 500); // Waiting half a bit to make sure each command will get through separately.
+
   butIbeacon.textContent = "Stop Beacon";
   butEddystone.classList.toggle("hidden", true);
   isIbeaconAdv = true;
@@ -136,10 +130,14 @@ function clickIbeacon() {
 
 /**
  * @name clickEddystone
- * Click handler for the connect/disconnect button.
+ * Click handler for the Eddystone Beacon button.
+ * Checks if an Eddystone beacon is already running by checking the boolean isEddystoneAdv.
+ * If isEddystoneAdv = true: Stops advertising, changes the button text and shows the iBeacon button. Finally sets isEddystoneAdv = false.
+ * If isEddystoneAdv = false: Sets the advertising data to setup an Eddystone beacon with a link to google.com and starts advertising.
+ * Also changes button text and hides the iBeacon button. Finally sets isEddystoneAdv = true.
  */
 function clickEddystone() {
-  console.log("EDDYSTONE BUTTON");
+  console.log("EDDYSTONE BUTTON PRESSED");
   if (isEddystonesAdv) {
     writeCmd("AT+ADVSTOP");
     butEddystone.textContent = "Make Eddystone Beacon";
@@ -150,7 +148,8 @@ function clickEddystone() {
   writeCmd("AT+ADVDATA=03:03:aa:fe 0d:16:aa:fe:10:00:03:67:6f:6f:67:6c:65:07");
   setTimeout(() => {
     writeCmd("AT+ADVSTART=0;200;3000;0;");
-  }, 500);
+  }, 500); // Waiting half a bit to make sure each command will get through separately.
+
   butIbeacon.classList.toggle("hidden", true);
   butEddystone.textContent = "Stop Beacon";
   isEddystonesAdv = true;
@@ -175,32 +174,17 @@ async function readLoop() {
 }
 
 /**
- * @name writeToStream
- * Gets a writer from the output stream and send the lines to the micro:bit.
- * @param  {...string} lines lines to send to the micro:bit
- */
-function writeToStream(...lines) {
-  // Write to output stream
-  const writer = outputStream.getWriter();
-  lines.forEach((line) => {
-    console.log("[SEND]", line);
-    writer.write(line + "\r");
-  });
-  writer.releaseLock();
-}
-
-/**
  * @name writeCmd
- * Gets a writer from the output stream and send the command to the ssd005 dongle.
- * @param  {string} cmd command to send to the ssd005 dongle
+ * Gets a writer from the output stream and send the command to the Smart USB Dongle 2.0.
+ * @param  {string} cmd command to send to the Smart USB Dongle 2.0
  */
 function writeCmd(cmd) {
   // Write to output stream
   const writer = outputStream.getWriter();
   console.log("[SEND]", cmd);
-  writer.write(cmd);
-  writer.write("\r");
 
+  writer.write(cmd);
+  writer.write("\r"); // Important to send a carriage return after a command
   writer.releaseLock();
 }
 
@@ -228,14 +212,18 @@ class LineBreakTransformer {
   }
 }
 
+/**
+ * @name toggleUIConnected
+ * Toggles the butIbeacon & butEddystone buttons visable/hidden depending on if dongle is connected or not.
+ * Also changes the text on butConnect depending on the action it actually will preform in the current state.
+ * @param  {boolean} connected true if connected, false if disconnected.
+ */
 function toggleUIConnected(connected) {
   let lbl = "Connect";
-  butIbeacon.classList.toggle("hidden", true);
-  butEddystone.classList.toggle("hidden", true);
   if (connected) {
-    butIbeacon.classList.toggle("hidden", false);
-    butEddystone.classList.toggle("hidden", false);
     lbl = "Disconnect";
   }
+  butIbeacon.classList.toggle("hidden", !connected);
+  butEddystone.classList.toggle("hidden", !connected);
   butConnect.textContent = lbl;
 }
